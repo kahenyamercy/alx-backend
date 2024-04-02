b708e4fe5c7319b93a3e7a7e744312ab9b2a0fa5
kahenyamercy <kahenyamercy5@gmail.com>
2024-04-02 13:18:57 +0300

    simple helper function that returs a tuple

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -17,4 +17,4 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return (start_index, end_index)
+    return start_index, end_index
